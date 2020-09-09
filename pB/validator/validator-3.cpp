f99a51da67ce7fdef3d1c11649f479a73770a1eb
xiplus <huangxuanyuxiplus@gmail.com>
2020-09-09 14:33:34 +0800

    Add pB validator

---
+++ pB/validator/validator-3.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+#include "testlib.h"
+
+using namespace std;
+
+const int _1e5 = 100000;
+const int _1e6 = 1000000;
+const int _1e9 = 1000000000;
+
+int main() {
+	registerValidation();
+
+	int N = inf.readInt(1, _1e5);
+	inf.readSpace();
+	int K = inf.readInt(1, _1e6);
+	inf.readSpace();
+	int M = inf.readInt(0, _1e9);
+	inf.readEoln();
+
+	int a = inf.readInt(1, _1e9);
+	ensure(a == 1);
+	for (int i = 1; i < N; i++) {
+		inf.readSpace();
+		int a = inf.readInt(1, _1e9);
+		ensure(a == 1);
+	}
+	inf.readEoln();
+
+	int b = inf.readInt(1, _1e9);
+	ensure(b == 1);
+	for (int i = 1; i < N; i++) {
+		inf.readSpace();
+		int b = inf.readInt(1, _1e9);
+		ensure(b == 1);
+	}
+	inf.readEoln();
+
+	inf.readEof();
+
+	return 0;
+}
